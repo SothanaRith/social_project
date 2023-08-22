bb00ddc4871c7c14febfb32de3af780e114e0996
SothanaRith <heangsothanarith@gmail.com>
2023-08-22 13:18:35 +0700

    first commit

---
+++ src/coponents/navber/navbar.jsx
@@ -0,0 +1,50 @@
+import "./navbar.scss"
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import WidgetsOutlinedIcon from '@mui/icons-material/WidgetsOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModecontext";
+import { AuthContext } from "../../context/authcontext";
+const Navbar = () => {
+
+    const { toggle, darkMode } = useContext(DarkModeContext);
+    const {currentUser} = useContext(AuthContext);
+    return (
+        <div className="navbar">
+            <div className="left">
+                <Link to="/" style={{ textDecoration: "none" }}><span>MANOREA</span></Link>
+                <HomeOutlinedIcon />
+                {darkMode ? (
+                    <LightModeIcon onClick={toggle} />
+                    
+                ) : (
+                    <DarkModeOutlinedIcon onClick={toggle} />
+                )}
+                <WidgetsOutlinedIcon />
+                <div className="search">
+                    <SearchOutlinedIcon />
+                    <input type="text" placeholder="Search" />
+                </div>
+
+            </div>
+            <div className="right">
+                <PersonOutlineOutlinedIcon />
+                <EmailOutlinedIcon />
+                <NotificationsNoneOutlinedIcon />
+                <div className="user">
+                    <img src={currentUser.profilePic} alt="" />
+                    <span>{currentUser.name}</span>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+export default Navbar;
